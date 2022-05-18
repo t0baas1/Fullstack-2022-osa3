23e03f94acb824706d40224f8e0b886ada6d6e92
Topi Kanninen <topi.kanninen@sci.fi>
2022-05-18 17:00:15 +0300

    delete fix

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.get('/api/persons/:id', (req, res) => {
 app.delete('/api/persons/:id', (req, res, next) => {
     Person.findByIdAndRemove(req.params.id)
         .then(result => {
-            response.status(204).end()
+            res.status(204).end()
         })
         .catch(error => next(error))
 })
